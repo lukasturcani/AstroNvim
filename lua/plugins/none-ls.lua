1e2938472abe83433cf14ba07c9ec9eccaa2fb29
Lukas Turcani <lukas@edra.ai>
2025-02-15 23:56:28 +0000

    Don't configure none ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,3 +1,5 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- Customize None-ls sources
 
 ---@type LazySpec
@@ -17,7 +19,7 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
-      null_ls.builtins.diagnostics.mypy,
+      -- null_ls.builtins.diagnostics.mypy,
     })
   end,
 }
